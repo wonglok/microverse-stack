288bde607d2f73238a1ff52a13d19f8e16981b75
Wong Lok <wonglok@users.noreply.github.com>
2023-03-19 16:30:45 +0800

    save

--- server.cjs
+++ server.cjs
@@ -1,10 +1,10 @@
 const express = require("express");
-const helmet = require("helmet");
+// const helmet = require("helmet");
 
 const app = express();
 const port = 3000;
 
-app.use(helmet());
+// app.use(helmet());
 app.use(express.static("dist"));
 
 app.get("/api", (req, res) => {
