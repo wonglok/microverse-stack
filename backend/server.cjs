35ed79b12a8021934f4923e8a140bc06c510fbee
Wong Lok <wonglok@users.noreply.github.com>
2023-03-26 18:32:17 +0800

    server

--- backend/server.cjs
+++ backend/server.cjs
@@ -80,7 +80,7 @@ db.mongoose
   })
   .then(() => {
     console.log("Successfully connect to MongoDB.");
-    initial();
+    initialSeed();
   })
   .catch((err) => {
     console.error("Connection error", err);
@@ -109,7 +109,7 @@ server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-function initial() {
+function initialSeed() {
   Role.estimatedDocumentCount((err, count) => {
     if (!err && count === 0) {
       new Role({
